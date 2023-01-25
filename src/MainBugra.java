726eafc2298e12065843e8fccfcfa62b19469a8b
gucerbuq <gbugrayalcin@gmail.com>
2023-01-25 09:48:52 +0200

    güncelleme

--- src/MainBugra.java
+++ src/MainBugra.java
@@ -1,6 +1,6 @@
 public class MainBugra {
     public static void main(String[] args) {
-        System.out.println("Hello Bugra!");
+        System.out.println("Hello Gucerbuq!");
         System.out.println("Hello world!");
     }
 }
