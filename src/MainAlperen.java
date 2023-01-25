e5861c8f92abeeb84b3cdf709a9eac1be71fc850
Alperen İkinci <alperen.ikinci@hotmail.com>
2023-01-25 11:07:04 +0300

    Commit 5

--- src/MainAlperen.java
+++ src/MainAlperen.java
@@ -3,5 +3,6 @@ public class MainAlperen {
     public static void main(String[] args) {
         System.out.println("Hello Alperen!");
         System.out.println("How are you?");
+        System.out.println("I'm fine thanks.");
     }
 }
